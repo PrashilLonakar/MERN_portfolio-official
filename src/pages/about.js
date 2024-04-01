0512cacb964352b67a5b7eeacb02d72492b4f017
prashil-lonakar <prashil.l@volgainfotech.com>
2024-04-01 10:45:33 +0530

    about page continue working

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Head from 'next/head';
 import AnimatedText from '@/components/AnimatedText';
 import Layout from '@/components/Layout';
@@ -41,13 +41,35 @@ const about = () => {
                             </p>
                         </div>
 
-                        <div className="col-span-3 relative h-max rounded-2*1 border-2 border-solid border-dark bg-light p-8">
-                            <div></div>
+                        <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
+                            <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark " />
                             <Image
                                 src={profilePic}
                                 alt="Codebucks"
                                 className="w-full h-auto rounded-2xl"
                             ></Image>
+                        </div>
+                        <div className="col-span-2 flex flex-col items-end justify-between">
+                            <div className="flex flex-col items-end justify-center">
+                                <span className="inline-block text-7xl font-bold">50+</span>
+                                <h2 className="text-xl font-medium capitalize text-dark/75">
+                                    satisfied clients
+                                </h2>
+                            </div>
+
+                            <div className="flex flex-col items-end justify-center">
+                                <span className="inline-block text-7xl font-bold">40+</span>
+                                <h2 className="text-xl font-medium capitalize text-dark/75">
+                                    projects completed
+                                </h2>
+                            </div>
+
+                            <div className="flex flex-col items-end justify-center">
+                                <span className="inline-block text-7xl font-bold">4+</span>
+                                <h2 className="text-xl font-medium capitalize text-dark/75">
+                                    years of experience
+                                </h2>
+                            </div>
                         </div>
                     </div>
                 </Layout>
