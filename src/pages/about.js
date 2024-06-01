9260114e517a82201b72a54c6aecd820d0b5fb79
prashil-lonakar <prashil.l@volgainfotech.com>
2024-06-02 00:08:35 +0530

    about page darkmode completed

--- src/pages/about.js
+++ src/pages/about.js
@@ -47,7 +47,7 @@ const about = () => {
           ></AnimatedText>
           <div className="grid w-full grid-cols-8 gap-16">
             <div className="col-span-3 flex flex-col items-start justify-start">
-              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                 Biography
               </h2>
               <p className="font-medium">
@@ -72,8 +72,8 @@ const about = () => {
               </p>
             </div>
 
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
-              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark " />
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid p-8 bg-light border-dark dark:bg-dark dark:border-light">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
               <Image
                 src={profilePic}
                 alt="Codebucks"
