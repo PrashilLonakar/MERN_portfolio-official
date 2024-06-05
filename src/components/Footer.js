022906daa6235de287657e0df655fbe069626fa3
prashil-lonakar <prashil.l@volgainfotech.com>
2024-06-05 19:00:17 +0530

    tab Title updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,29 @@
-import React from "react";
-import Layout from "./Layout";
-import Link from "next/link";
+import React from 'react';
+import Layout from './Layout';
+import Link from 'next/link';
 
 const Footer = () => {
-  return (
-    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light">
-      <Layout className="py-8 flex items-center justify-between">
-        <span className="">
-          {new Date().getFullYear()} &copy; All Right Reserved.
-        </span>
-        <div className="flex items-center">
-          Build with{" "}
-          <span className="text-primary text-2xl px-1 dark:text-primaryDark">
-            {" "}
-            &#9825;
-          </span>
-          by&nbsp;
-          <Link href="/" className="underline underline-offset-2">
-            CodeBucks
-          </Link>
-        </div>
-        <Link
-          className="underline underline-offset-2"
-          href="/"
-          target={"_blank"}
-        >
-          Say hello
-        </Link>
-      </Layout>
-    </footer>
-  );
+    return (
+        <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light">
+            <Layout className="py-8 flex items-center justify-between">
+                <span className="">{new Date().getFullYear()} &copy; All Right Reserved.</span>
+                <div className="flex items-center">
+                    Build with{' '}
+                    <span className="text-primary text-2xl px-1 dark:text-primaryDark">
+                        {' '}
+                        &#9825;
+                    </span>
+                    by&nbsp;
+                    <Link href="/" className="underline underline-offset-2">
+                        Prashlons
+                    </Link>
+                </div>
+                <Link className="underline underline-offset-2" href="/" target={'_blank'}>
+                    Say hello
+                </Link>
+            </Layout>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -82,7 +82,7 @@ const projects = () => {
     return (
         <>
             <Head>
-                <title>CodeBucks | Projects Page</title>
+                <title>Prashlons | Projects Page</title>
                 <meta name="description" content="any description"></meta>
             </Head>
             <main className="w-full mb-16 flex flex-col items-center justify-center">
